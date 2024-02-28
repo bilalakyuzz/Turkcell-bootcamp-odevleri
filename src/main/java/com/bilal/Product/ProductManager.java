a042660eef3a068c6236a851834b1a49ec41f702
bilalakyuzz <bilalakyuzz@gmail.com>
2024-02-28 15:12:01 +0300

    Turkcell gun 2 odev
    Turkcell bootcampinde verilen 2. gun odevidir

---
+++ src/main/java/com/bilal/Product/ProductManager.java
@@ -0,0 +1,10 @@
+package com.bilal.Product;
+
+
+import com.bilal.Product.Product;
+
+public class ProductManager {
+    public void add(Product product){
+        System.out.println("Product has been added "+ product.getName());
+    }
+}
---
+++ src/main/java/com/bilal/ReCapDemo/DortIslemMain.java
@@ -0,0 +1,9 @@
+package com.bilal.ReCapDemo;
+
+public class DortIslemMain {
+    public static void main(String[] args){
+        DortIslem dortIslem=new DortIslem();
+        int sonuc= dortIslem.Topla(3,4);
+        System.out.println(sonuc);
+    }
+}
